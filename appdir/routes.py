545330bd27abfdc950efc39ecd45e3e6813acf36
DanWilliams91 <danielwillliams1991@gmail.com>
2022-07-20 18:29:32 +0000

    Correct errors on routes.py

--- appdir/routes.py
+++ appdir/routes.py
@@ -2,11 +2,7 @@ from flask import flash, render_template, request, redirect, session, url_for
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 from appdir import app, db, mongo
-from appdir.models import (
-    Name,
-    Category,
-    Users
-)
+from appdir.models import Name, Category, Users
 
 
 @app.route("/", methods=["GET", "POST"])
