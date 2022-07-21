696941d4aba2ad5ae418fcc2b6f1c4afb54d7f7c
DanWilliams91 <danielwillliams1991@gmail.com>
2022-07-21 15:02:06 +0000

    Add data to test correct postings to Postgres database

--- appdir/models.py
+++ appdir/models.py
@@ -21,6 +21,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    is_superuser = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self):
         return self.user_name
--- appdir/routes.py
+++ appdir/routes.py
@@ -7,4 +7,12 @@ from appdir.models import Name, Category, Users
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("base.html")
+    admin_user = Users(
+        user_name = "Admin",
+        password = "ABC123",
+        is_superuser = True
+    )
+    db.session.add(admin_user)
+    db.session.commit()
+    users = list(User.query.order_by(User.id).all())
+    return render_template("base.html", users=users)
