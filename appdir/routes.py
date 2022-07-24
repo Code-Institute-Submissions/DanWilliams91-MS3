5c24f7299a0e6442b13b3db9ea21179ec6cfb8a2
DanWilliams91 <danielwillliams1991@gmail.com>
2022-07-24 15:16:19 +0000

    Create login function and draft profile.html page.

--- appdir/routes.py
+++ appdir/routes.py
@@ -16,11 +16,12 @@ def register():
     if request.method == "POST":
         
         # check if username already exists in db
-        existing_user = Users.query.filter(Users.user_name == \
-            request.form.get("username").lower()).all()
+        existing_user = Users.query.filter(
+            Users.user_name == request.form.get("username").lower()).all()
 
         if existing_user:
-            flash("Username already exists. Please try another username or log in if you already have an account.")
+            flash("Username already exists. Please try another username or \
+                log in if you already have an account.")
             return redirect(url_for("register"))
 
         user = Users(
@@ -31,4 +32,58 @@ def register():
         db.session.add(user)
         db.session.commit()
 
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful! Welcome, {}.".format(
+            request.form.get("username")))
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+        
+    if "user" in session:
+        
+        return render_template("profile.html", username=session["user"])
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out successfully.")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = Users.query.filter(
+            Users.user_name == request.form.get("username").lower()).all()
+
+        if existing_user:
+            print(request.form.get("username"))
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user[0].password, request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Login successful. Welcome back, {}!".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
